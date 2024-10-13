78c91ebb54b7f8701520830b3f0d35727b56f3ef
nihilism011 <nihilism011@gmail.com>
2024-10-14 02:13:06 +0900

    리액트 antd 추가함

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Fruit from "./components/listTest.js";
 import Form from "./components/formTest.js";
+import SignupPage from "./components/signupPage.js";
 function App() {
   return (
     <div className="App">
       <Fruit list={["apple", "banana", 3]} />
       <Form />
       <div>hello world</div>
+      <SignupPage />
     </div>
   );
 }
---
+++ src/components/signupPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button, Input, Form, Radio, Card, Select } from "antd";
+
+const SignupPage = () => {
+  return <div>signupPage</div>;
+};
+
+export default SignupPage;
