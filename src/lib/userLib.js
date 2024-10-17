86ba5a3abaae5ce3449e20653d2655e153ca703e
nihilism011 <nihilism011@gmail.com>
2024-10-17 14:18:41 +0900

    대문자

--- src/lib/userLib.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export async function updateName(id, name) {
-  const url = `/test/${id}`;
-  const res = await axios.patch(url, { id: id, name: name });
-  return res;
-}
